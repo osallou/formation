3dbda9068763cd829b8403e7cc404829924e8ebf
Olivier Sallou <olivier.sallou@irisa.fr>
2012-09-06 11:31:06 +0200

    add fitnesse

---
+++ src/main/java/formation/code/PockerFixture.java
@@ -0,0 +1,14 @@
+package formation.code;
+
+import fit.ColumnFixture;
+
+public class PockerFixture extends ColumnFixture {
+
+	public float numerator;
+	public float denominator;
+	
+	public float quotient() {
+        return numerator/denominator;
+   }
+	
+}
